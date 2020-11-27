666aea9f0f69184b9d1ba9202641bba13f4a0087
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2020-11-27 18:39:43 +0200

    Added threadpool from java to compare, added more tasks for the thread pool

--- ThreadPoolProject/src/com/company/Main.java
+++ ThreadPoolProject/src/com/company/Main.java
@@ -1,15 +1,80 @@
 package com.company;
 
+import java.util.concurrent.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        Thread_Pool_Executor thread_pool = new Thread_Pool_Executor(12, 12, 1, 5);
+        int corePoolSize = 3;
+        int maximumPoolSize = 6;
+        int keepAliveTime = 5;
+        TimeUnit timeUnit = TimeUnit.SECONDS;
+        int queueSize = 6;
+        int i;
 
-        for (int i = 0; i < 50; i++) {
+        BlockingQueue<Runnable> queue=new LinkedBlockingQueue(queueSize);
+
+        Thread_Pool_Executor thread_pool = new Thread_Pool_Executor(corePoolSize, maximumPoolSize, keepAliveTime, queueSize);
+
+        //ThreadPoolExecutor thread_pool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, timeUnit, queue);
+
+        for (i = 0; i < 50; i++) {
             Pool_Task task = new Pool_Task(i);
-            thread_pool.execute(task);
+
+            try {
+                thread_pool.execute(task);
+            }
+            catch (RejectedExecutionException e) {
+                System.out.println("[" + i + "] > Task rejected");
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
-        thread_pool.shutdown();
+        System.out.println("Task count = " + thread_pool.getTaskCount());
+        System.out.println("Pool size = " + thread_pool.getPoolSize());
+
+        for (i = 50; i < 100; i++) {
+            Pool_Task task = new Pool_Task(i);
+
+            try {
+                thread_pool.execute(task);
+            }
+            catch (RejectedExecutionException e) {
+                System.out.println("[" + i + "] > Task rejected");
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Task count = " + thread_pool.getTaskCount());
+        System.out.println("Pool size = " + thread_pool.getPoolSize());
+
+        for (i = 100; i < 150; i++) {
+            Pool_Task task = new Pool_Task(i);
+
+            try {
+                thread_pool.execute(task);
+            }
+            catch (RejectedExecutionException e) {
+                System.out.println("[" + i + "] > Task rejected");
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Task count = " + thread_pool.getTaskCount());
+        System.out.println("Pool size = " + thread_pool.getPoolSize());
+
+        //thread_pool.shutdown();
     }
 }
