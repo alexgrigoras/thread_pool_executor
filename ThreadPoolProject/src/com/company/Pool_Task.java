7fdde77892c4e9284c408f302bd390fabb15da21
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2020-12-15 17:25:30 +0200

    Added task delay

--- ThreadPoolProject/src/com/company/Pool_Task.java
+++ ThreadPoolProject/src/com/company/Pool_Task.java
@@ -2,17 +2,20 @@ package com.company;
 
 public class Pool_Task implements Runnable {
     private final int id;
+    private final int delay;
 
-    public Pool_Task(int task_number) {
+    public Pool_Task(int task_number, int delayMs) {
         id = task_number;
+        delay = delayMs;
     }
 
     public void run() {
+        System.out.println("[" + id + "] > Task running");
+
         try {
-            Thread.sleep(2000);
+            Thread.sleep(delay);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("[" + id + "] > Task running");
     }
 }
